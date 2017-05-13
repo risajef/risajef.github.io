a2842ad8b2dabbb3aa2ecf9834cc2f905c6bf503
risajef <reto.a.weber@gmail.com>
2017-05-13 17:53:11 +0200

    php test1

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h1>This is my homepage about my <a href="https://github.com/risajef">GitHub</a>.
+        </h1>
+        <p>Feel free to just got to the GitHub.</p>
+        <p>
+            <p <?php echo("style=\"visibility: hidden\""); ?>>This text is only shown if php does not work.</p>
+            <p><?php echo("This text is only shown if php does work."); ?></p>
+        </p>
+    </body>
+</html>
